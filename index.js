30b3bf6f18801b755e612716f04354d99270929b
Ahmedmo7 <43525160+Ahmedmo7@users.noreply.github.com>
2019-10-19 17:27:42 -0400

    login working

--- index.js
+++ index.js
@@ -74,18 +74,3 @@ function logout(){
         });
         // [END createwithemail]  
 }
-
-    /**
-     * Sends an email verification to the user.
-     */
-    function sendEmailVerification() {
-        // [START sendemailverification]
-        firebase.auth().currentUser.sendEmailVerification().then(function() {
-          // Email Verification sent!
-          // [START_EXCLUDE]
-          alert('Email Verification Sent!');
-          // [END_EXCLUDE]
-        });
-        // [END sendemailverification]
-      }
-  
